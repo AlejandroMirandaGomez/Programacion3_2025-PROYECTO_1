ce7d3a6b7dfad686e08efa00fd364aa5cd7c302b
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2025-08-22 22:40:14 -0600

    Arreglando Hola.java

--- src/Nando/Hola.java
+++ src/Nando/Hola.java
@@ -3,7 +3,7 @@ package Nando;
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("ADios mundo");
+        System.out.println("Adios mundo");
         System.out.println("Queeeeee");
     }
 }
