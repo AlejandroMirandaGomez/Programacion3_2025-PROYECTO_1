64788c844a6d9e8e0ef6c6fbef7fc6761dbe637e
arnaldojarab <arnaldo.jara.bartels@est.una.ac.cr>
2025-08-23 17:00:33 -0600

    Add class Medico

---
+++ src/main/java/proyecto_1/data/Medico.java
@@ -0,0 +1,39 @@
+package main.java.proyecto_1.data;
+
+public class Medico {
+    private String id;
+    private String nombre;
+    private String especialidad;
+    private String password;
+
+    public  Medico(String id, String nombre, String especialidad, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.password = password;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getEspecialidad() {
+        return especialidad;
+    }
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+}
